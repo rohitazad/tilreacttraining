c2f0fb04a6ba220b38bf1e615f24009c2a5a1d24
rohitazad <ram9953878727@gmail.com>
2024-05-29 15:16:17 +0530

    api code done

--- src/Utils/common.js
+++ src/Utils/common.js
@@ -2,6 +2,6 @@ import { json } from 'react-router-dom';
 
 export const GETAPIData = async ({ url }) => {
   const data = await fetch(`${url}`);
-  //const resData = await data.json();
-  return data;
+  const resData = await data.json();
+  return resData;
 };
